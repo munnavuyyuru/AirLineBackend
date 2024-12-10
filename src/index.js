8cd52987eb6cc837ff897f190ab494905073c686
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2024-12-10 16:38:02 +0530

    db setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const { PORT } = require("./config/serverConfig");
 
 const setUpServer = async () => {
   const app = express();
-  const PORT = 3000;
 
   app.listen(PORT, () => {
     console.log(`server started at ${PORT}`);
