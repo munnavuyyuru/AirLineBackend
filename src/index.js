0627ebafcac814c27897f49c7a2bfd6f6f63ea4b
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2024-12-15 13:15:10 +0530

    added router to create Api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-// const CityRepository = require("./repository/city-Repository");
+const ApiRoutes = require("./routes/index");
 
 const setUpServer = async () => {
   const app = express();
@@ -10,10 +10,10 @@ const setUpServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.use("/api", ApiRoutes);
+
   app.listen(PORT, async () => {
     console.log(`server started at ${PORT}`);
-    // const repo = new CityRepository();
-    // repo.createCity({ name: "Delhi" });
   });
 };
 
